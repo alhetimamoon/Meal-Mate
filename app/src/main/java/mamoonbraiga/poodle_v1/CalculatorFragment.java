550e3a22fdb4e6aab57b84c2fbd61c6c8e13397a
MamoonBraiga <mamoon.alheti@gmail.com>
2015-10-28 13:43:24 -0400

    Added the initial calculator fragment design.

--- app/src/main/java/mamoonbraiga/poodle_v1/CalculatorFragment.java
+++ app/src/main/java/mamoonbraiga/poodle_v1/CalculatorFragment.java
@@ -1,9 +1,58 @@
 package mamoonbraiga.poodle_v1;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import mamoonbraiga.poodle_v3.R;
 
 /**
  * Created by MamoonBraiga on 2015-10-25.
  */
-public class CalculatorFragment extends Fragment{
+public class CalculatorFragment extends Fragment implements View.OnClickListener{
+
+        private Button bmrButton;
+        private TextView bmrValue;
+        private EditText weight, height, age;
+        private int bmr = 0;
+
+        public Fragment newInstance(Context context) {
+            CalculatorFragment f = new CalculatorFragment();
+
+            return f;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+            ViewGroup root = (ViewGroup) inflater.inflate(R.layout.calculator_layout, null);
+            intit(root);
+            return root;
+        }
+    private void intit(ViewGroup root) {
+        bmrButton = (Button) root.findViewById(R.id.bmrButton);
+        weight = (EditText) root.findViewById(R.id.eWeight);
+        height = (EditText) root.findViewById(R.id.eHeight);
+        age = (EditText) root.findViewById(R.id.eAge);
+        bmrValue = (TextView) root.findViewById(R.id.bmrValue);
+        bmrButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bmrButton:
+                bmr = (int) (66 + (13.7 * Integer.parseInt(weight.getText().toString())) + (5 * Integer.parseInt(height.getText().toString())) - (6.8 * Integer.parseInt(age.getText().toString())));
+        }
+        bmrValue.setText(Integer.toString(bmr));
+    }
 }
